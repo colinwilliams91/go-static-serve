c98a84cc3ed19e14cc0c8b79e2bd7aa9fa322b20
Colin Williams <colin.williams.dev@gmail.com>
2024-09-04 23:12:06 -0500

    :recycle:: rm fmt for now, comment options for serving dirs, static files, writing html to http.ResponseWriter

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -12,8 +11,12 @@ import (
 */
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "static")
+
 	// TODO: modular renderPage function (renders views? renders raw html passed in?)
+
+	// TODO: 3 options for routing (writing http...)
 	// err := ...
 	// w.Write()
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
